4e2a43b2662004c9cfeadca586632899495f92c8
Gaurav <mail.gauravgol@mobzway.in>
2024-07-05 11:09:37 +0530

    Add Content in Project Section

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReact, faNodeJs, faJsSquare, faHtml5, faCss3Alt, faGit, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { CSSTransition } from 'react-transition-group';
+
 
 function Profile() {
+    const [showMessage, setShowMessage] = useState(false);
+
+    useEffect(() => {
+        setShowMessage(true);
+    }, []);
+
     return (
         <Container fluid className="p-5 bg-light">
             <Row className="align-items-center">
                 <Col md={6} className="text-center text-md-left mb-4 mb-md-0">
-                    <h1 className="display-4">I am Gaurav, Software Developer</h1>
+                    <CSSTransition
+                        in={showMessage}
+                        timeout={5000}
+                        classNames="fade"
+                        unmountOnExit
+                    >
+                        <div>
+                            <h1 className="display-4">Welcome</h1>
+                            <h1 className="display-4">I am Gaurav, Software Developer</h1>
+                        </div>
+                    </CSSTransition>
                 </Col>
                 <Col md={6} className="text-center">
+
                     <img src='./dev.webp' alt="Profile" className="img-fluid rounded" style={{ maxWidth: '50%', height: '20%' }} />
                 </Col>
             </Row>
             <Row className="mt-5">
                 <Col className="text-center">
-
                     <div className="d-flex justify-content-center flex-wrap mt-3">
                         <FontAwesomeIcon icon={faReact} size="3x" className="m-3" color="#61DBFB" />
                         <FontAwesomeIcon icon={faNodeJs} size="3x" className="m-3" color="#68A063" />
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,44 +2,65 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
+
 function Projects() {
     return (
-        <Container fluid className="p-5 bg-light">
-            <Row className="align-items-center mb-4">
-                <Col md={12} className="text-center">
-                    <h1 className="display-4">Projects</h1>
+        <Container fluid>
+            <h1 className="text-center custom-heading">Projects</h1>
+            <Row className="justify-content-center">
+                <Col xs={12} md={4} className="mb-4">
+                    <h4 className="text-center">ADMIN PANEL | For a betting site</h4>
+                    <img src="./adminpanel.png" className="img-fluid" alt="Admin Panel" />
+                    <p>This production-level admin panel was built for my client and is a live project built on React. It provides comprehensive tools to manage a betting site. Key features include:</p>
+                    <ul>
+                        <li>Player Management: View player details and activity.</li>
+                        <li>Transaction Approval: Approve and monitor withdrawal requests.</li>
+                        <li>Dashboard Reporting: Real-time data visualization and reporting.</li>
+                        <li><a href='https://cliq.zoho.com/company/654200107/chats/CT_2231796882690347967_654200107-B1/messages/1720154931280_131017031760'>Live Link</a></li>
+                        <li><a href='https://github.com/Gauravgol/GroomAdminPanel'>Details || code</a></li>
+                    </ul>
+                </Col>
+                <Col xs={12} md={4} className="mb-4">
+                    <h4 className="text-center">BACKEND FOR VIDEO STREAMING SITE</h4>
+                    <img src="./adminpanel.png" className="img-fluid" alt="Backend for Video Streaming Site" />
+                    <p>This is the backend for a video streaming site built using Node.js and MongoDB. It features production-level backend practices, JWT tokens for authentication, Cloudinary for video storage, and bcrypt for password encryption. Key features include:</p>
+                    <ul>
+                        <li>User registration: Approve and monitor user accounts.</li>
+                        <li>Login, logout: Manage user sessions securely.</li>
+                        <li>Image, video upload: Support for multimedia content uploads.</li>
+                        <li>Subscribe, Like, Comment: perform all these events.</li>
+                        <li><a href='https://github.com/Gauravgol/Backend-Project'>API Documentation</a></li>
+                        <li><a href='https://github.com/Gauravgol/Backend-Project'>Details || code</a></li>
+                    </ul>
+                </Col>
+                <Col xs={12} md={4} className="mb-4">
+                    <h4 className="text-center">SOCKET IO PROJECT</h4>
+                    <img src="./socketTask.png" className="img-fluid" alt="Socket IO Project" />
+                    <p>In this project, I have created a form to save data in MongoDB. Users can fetch their details and use Socket.IO to see all the live users.</p>
+                    <ul>
+                        <li>User registration: Approve and monitor user accounts.</li>
+                        <li>Login, logout: Manage user sessions securely.</li>
+                        <li>Image, video upload: Support for multimedia content uploads.</li>
+                        <li>Subscribe, Like, Comment: perform all these events.</li>
+                        <li><a href='https://task-1and-2.onrender.com'>Live Link</a></li>
+                        <li><a href='https://github.com/Gauravgol/Task2'>Details || code</a></li>
+                    </ul>
                 </Col>
             </Row>
             <Row className="justify-content-center">
-                <Col md={8}>
-                    <Card className="mb-4">
-                        <Card.Body>
-                            <Card.Title>ADMIN PANEL | For a betting site</Card.Title>
-                            <Card.Text>
-                                This production-level admin panel was built for my client and is a live project built on React. It provides comprehensive tools to manage a betting site. Key features include:
-                            </Card.Text>
-                            <ul>
-                                <li>Player Management: View player details and activity.</li>
-                                <li>Transaction Approval: Approve and monitor withdrawal requests.</li>
-                                <li>Dashboard Reporting: Real-time data visualization and reporting.</li>
-                            </ul>
-                            <Card.Text>
-                                The frontend utilizes React, HTML, CSS, and Bootstrap for responsive design. This admin panel ensures efficient management and secure operations for the betting platform.
-                            </Card.Text>
-                            <Button variant="primary" href="https://github.com/yourusername/project-repo" target="_blank">GitHub Source Code</Button>
-                        </Card.Body>
-                    </Card>
+                <Col xs={12} md={12} className="mb-4">
+                    <h4 className="text-center"> PortFoilio site</h4>
 
-                    <Card className="mb-4">
-                        <Card.Body>
-                            <Card.Title>BACKEND FOR VIDEO STREAMING SITE</Card.Title>
-                            <Card.Text>
-                                This is the backend for a video streaming site built using Node.js and MongoDB. It features production-level backend practices, JWT tokens for authentication, Cloudinary for video storage, and bcrypt for password encryption.
-                            </Card.Text>
-                            <Button variant="primary" href="https://github.com/yourusername/project-repo" target="_blank">GitHub Source Code</Button>
-                        </Card.Body>
-                    </Card>
+                    <p>
+                        Welcome to my portfolio site! As you are now seeing my portfolio project. This platform is built using React and showcases my work and projects. I  continuously update the site to reflect my latest developments and achievements. I have used bootsrap and some npm packages and future i will integrated custom APIs and implement my learing here
+                    </p>
+                    <ul>
+
+
+                        <li><a href='https://github.com/Gauravgol/PortfolioApp'>Details || code</a></li>
+                    </ul>
                 </Col>
+
             </Row>
         </Container>
     );
